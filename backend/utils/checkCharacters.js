eb3730743a8e1adb4439c85aac647c8304b13a12
WayneGaming <92593052+WayneGaming01@users.noreply.github.com>
2023-06-08 11:07:22 +0800

    Update checkCharacters.js

--- backend/utils/checkCharacters.js
+++ backend/utils/checkCharacters.js
@@ -1,32 +1,6 @@
-const generateCharacters = (length) => {
-  let result = "";
-  const characters =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-};
-
 const containsSpecialChars = (str) => {
   const specialChars = /[`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
   return specialChars.test(str);
 }
 
-const generateNumbers = (length) => {
-  let result = "";
-  const characters =
-    "0123456789";
-  const charactersLength = characters.length;
-  let counter = 0;
-  while (counter < length) {
-    result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    counter += 1;
-  }
-  return result;
-};
-
-module.exports = { generateCharacters, generateNumbers, containsSpecialChars };
+module.exports = { containsSpecialChars };
